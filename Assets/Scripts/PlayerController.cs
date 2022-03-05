db5b9aa4944c6158445594f7cc9a926ece10ce42
Abby <abbydiode@gmail.com>
2022-03-05 23:27:52 +0100

    Add basic jumping implementation #1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float WalkSpeed = 5f;
+    public float JumpHeight = 10f;
     Transform transform;
     void Start() {
         transform = GetComponent<Transform>();
     }
 
     void FixedUpdate() {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * Time.deltaTime;
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * Time.fixedDeltaTime;
+
+        if (Input.GetButton("Jump")) {
+            transform.position += new Vector3(0, JumpHeight, 0) * Time.fixedDeltaTime;
+        }
     }
 }
