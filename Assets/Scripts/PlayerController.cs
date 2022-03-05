f93c16b5d923e7cd6cac82efd74511870b2a69f0
Abby <abbydiode@gmail.com>
2022-03-05 23:22:05 +0100

    Add basic walking mechanics #1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+    public float WalkSpeed = 5f;
+    Transform transform;
+    void Start() {
+        transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * Time.deltaTime;
     }
 }
