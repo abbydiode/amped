fa3cf0dc875597e56d744facc08296d47f881f6a
Abby <abbydiode@gmail.com>
2022-03-06 15:25:58 +0100

    Simplify accessing player transform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float WalkSpeed = 5f;
     public float JumpHeight = 10f;
-    Transform transform;
-    void Start() {
-        transform = GetComponent<Transform>();
-    }
 
     void FixedUpdate() {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * Time.fixedDeltaTime;
