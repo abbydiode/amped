2d4aef14a6b8c101507630d2a41e5754c656c490
Abby <abbydiode@gmail.com>
2022-03-06 15:33:16 +0100

    Add air control slider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float WalkSpeed = 5f;
-    public float JumpHeight = 10f;
+    public float WalkSpeed = 5;
+    public float JumpHeight = 10;
+    [Range(0, 1)] public float AirControlMultiplier = 0.5f;
 
     void FixedUpdate() {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * Time.fixedDeltaTime;
