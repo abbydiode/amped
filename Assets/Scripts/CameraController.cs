18a1220efa471fdc7dfd49b2de0d091bfb540efa
Abby <abbydiode@gmail.com>
2022-03-08 21:23:43 +0100

    Add camera smoothing to main camera #15

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     public float VerticalOffset = 2;
-    Transform player;
+    [Range(0, 0.5f)] public float SmoothingTime = 0.1f;
+    private Transform player;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void FixedUpdate() {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + VerticalOffset, transform.position.z);
+        Vector3 target = new Vector3(player.transform.position.x, player.transform.position.y + VerticalOffset, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, SmoothingTime, Mathf.Infinity, Time.fixedDeltaTime);
     }
 }
