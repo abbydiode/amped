2e40a2cae5d01d14f5d2e9aaebd903f2002ab44e
Abby <abbydiode@gmail.com>
2022-03-08 21:42:10 +0100

    Update player controller to work better with built-in rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float WalkSpeed = 5;
-    public float JumpForce = 150;
+    public float JumpForce = 3;
     public Transform FeetPosition;
     [Range(0, 1)] public float AirControlMultiplier = 0.75f;
     private Rigidbody2D _rigidbody;
@@ -15,13 +15,18 @@ public class PlayerController : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * WalkSpeed, 0, 0) * (isGrounded ? 1 : AirControlMultiplier) * Time.fixedDeltaTime;
+        Vector2 newVelocity = _rigidbody.velocity;
+
         _spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
 
+        isGrounded = Physics2D.Raycast(FeetPosition.position, Physics.gravity.normalized, 0.1f, ~LayerMask.GetMask("Player")).collider != null;
+
         if (Input.GetButton("Jump") && isGrounded) {
-            _rigidbody.AddForce(Vector2.up * JumpForce);
+            newVelocity.y += JumpForce;
         }
 
-        isGrounded = Physics2D.Raycast(FeetPosition.position, Vector2.down, 0.1f, ~LayerMask.GetMask("Player")).collider != null;
+        newVelocity.x = Input.GetAxis("Horizontal") * WalkSpeed * (isGrounded ? 1 : AirControlMultiplier);
+        
+        _rigidbody.velocity = newVelocity;
     }
 }
