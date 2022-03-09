83c0d48dfe9a67a6dd195e88765131a73bb0b2fb
Tymo <39733374+Strike2610@users.noreply.github.com>
2022-03-09 17:12:03 +0100

    Shoot grappling line according to Aim
    Shoot a preliminary grappling line according to crosshair location.
    Press R2 to shoot and release to retract.

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -2,17 +2,41 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
+    public float ExtensionSpeed = 0.01f;
+
     float x, y;
+    bool IsAiming, Shoot;
+    public GameObject Arm, Crosshair;
+    SpriteRenderer ArmRenderer;
+
+    void Start()
+    {
+        ArmRenderer = Arm.GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         x = Input.GetAxisRaw("AimHorizontal");
         y = Input.GetAxisRaw("AimVertical");
-        transform.localPosition = new Vector3(
+        Shoot = Input.GetAxisRaw("ShootHook") < -0.5;
+
+        IsAiming = Mathf.Abs(x) + Mathf.Abs(y) > 0.5;
+
+        Crosshair.transform.localPosition = new Vector3(
             x * Mathf.Sqrt(1 - 0.5f * y * y),
             y * Mathf.Sqrt(1 - 0.5f * x * x)
         );
 
-        //this.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, distance);
+        Arm.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(y, x) * (180 / Mathf.PI));
+
+        Crosshair.GetComponent<SpriteRenderer>().material.color = new Color(1, 1, 1, IsAiming ? 1 : 0);
+
+
+        if (Shoot)
+            ArmRenderer.size += new Vector2(ExtensionSpeed / 2, 0);
+        else if (ArmRenderer.size.x > 0.1)
+            ArmRenderer.size -= new Vector2(ExtensionSpeed / 2, 0);
+
+        ArmRenderer.material.color = new Color(1, 1, 1, ArmRenderer.size.x > 0.5f ? 1 : 0);
     }
 }
