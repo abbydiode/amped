21eb217ec44d4d9f6eb1a35667ef8e2511842fd7
Abby <abbydiode@gmail.com>
2022-03-21 19:44:26 +0100

    #18 Disable gravity and remove grounded checks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,31 +2,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float WalkSpeed = 5;
-    public float JumpForce = 3;
-    public Transform FeetPosition;
-    [Range(0, 1)] public float AirControlMultiplier = 0.75f;
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
-    private bool isGrounded = false;
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
-    void FixedUpdate() {
-        Vector2 newVelocity = _rigidbody.velocity;
+    async void FixedUpdate() {
+        Vector2 newPosition = _rigidbody.position + new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * WalkSpeed * Time.fixedDeltaTime;
 
         _spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
+        _spriteRenderer.flipY = Input.GetAxisRaw("Vertical") < 0;
 
-        isGrounded = Physics2D.Raycast(FeetPosition.position, Physics.gravity.normalized, 0.1f, ~LayerMask.GetMask("Player")).collider != null;
-
-        if (Input.GetButton("Jump") && isGrounded) {
-            newVelocity.y += JumpForce;
-        }
-
-        newVelocity.x = Input.GetAxis("Horizontal") * WalkSpeed * (isGrounded ? 1 : AirControlMultiplier);
-        
-        _rigidbody.velocity = newVelocity;
+        _rigidbody.MovePosition(newPosition);
     }
 }
