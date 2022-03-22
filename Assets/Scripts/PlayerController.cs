bdd518d64484c48528344cad5d063d478602a6d1
Abby <abbydiode@gmail.com>
2022-03-22 20:13:38 +0100

    Implement animator in player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,22 @@ public class PlayerController : MonoBehaviour {
     public float WalkSpeed = 5;
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate() {
-        Vector2 newPosition = _rigidbody.position + new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * WalkSpeed * Time.fixedDeltaTime;
-
-        _spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
-        _spriteRenderer.flipY = Input.GetAxisRaw("Vertical") < 0;
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 newPosition = _rigidbody.position + movementVector  * WalkSpeed * Time.fixedDeltaTime;
 
         _rigidbody.MovePosition(newPosition);
+
+        _animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        _animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        _animator.SetFloat("Velocity", movementVector.magnitude);
     }
 }
