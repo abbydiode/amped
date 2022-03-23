4f1d047aa4c18a8aba9452ec8fe0777debbc3ca9
Abby <abbydiode@gmail.com>
2022-03-23 19:34:06 +0100

    Remove unused code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,17 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
-    private InputAction _moveAction;
     private Vector2 movementDirection;
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
-        //_moveAction = GetComponent<PlayerInput>();
     }
 
     void FixedUpdate() {
-        //Vector2 movementDirection = Vector2.ClampMagnitude(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), 1);
         Vector2 newPosition = _rigidbody.position + movementDirection  * WalkSpeed * Time.fixedDeltaTime;
-
         _rigidbody.MovePosition(newPosition);
-
-        //_animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        //_animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-        //_animator.SetFloat("Velocity", movementDirection.magnitude);
     }
 
     void OnMove(InputValue input) {
